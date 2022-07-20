c601fdea7ae01d9223f3b88c974457571ce09333
jsabariganes@vmware.com <jsabariganes@vmware.com>
2022-07-20 05:24:23 +0000

    Modifying vetList.html with current epoch time

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-return "Greetings from Spring Boot + Tanzu as of 1658293746!";
+return "Greetings from Spring Boot + Tanzu as of 1658294663!";
 	}
 
 }
